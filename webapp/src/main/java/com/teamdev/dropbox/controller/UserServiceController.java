a23ae058f6c627efa5ee0b47514d0c97bc037649
Bogdan Kovalev <bogdan.kovalev@teamdev.com>
2016-10-27 23:17:10 +0300

    user login REST API

--- webapp/src/main/java/com/teamdev/dropbox/controller/UserServiceController.java
+++ webapp/src/main/java/com/teamdev/dropbox/controller/UserServiceController.java
@@ -1,7 +1,10 @@
 package com.teamdev.dropbox.controller;
 
 import com.teamdev.dropbox.dto.UserDTO;
+import com.teamdev.dropbox.serviceobjects.AuthenticationToken;
+import com.teamdev.dropbox.serviceobjects.UserLoginInfo;
 import com.teamdev.dropbox.serviceobjects.UserRegistrationInfo;
+import com.teamdev.dropbox.services.UserAuthenticationService;
 import com.teamdev.dropbox.services.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +23,17 @@ public class UserServiceController {
     @Autowired
     UserRegistrationService userRegistrationService;
 
+    @Autowired
+    UserAuthenticationService userAuthenticationService;
+
     @PostMapping
     public UserDTO register(@RequestBody UserRegistrationInfo registrationData) throws Exception {
         return this.userRegistrationService.register(registrationData);
+    }
+
+    @PostMapping
+    @RequestMapping("/login")
+    public AuthenticationToken login(@RequestBody UserLoginInfo loginInfo) throws Exception {
+        return this.userAuthenticationService.login(loginInfo);
     }
 }
